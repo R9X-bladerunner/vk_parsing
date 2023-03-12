55f5becd3f84954230c3b16f6ee5a00452852dae
air1992 <dndrv12345@gmail.com>
2023-03-12 14:09:31 +0800

    upd1

---
+++ vk_parser/vk_parsing.py
@@ -0,0 +1,48 @@
+import time
+import csv
+
+import vk
+
+
+def authorize_in_app(login: str, password: str):
+    client_id = '51577778'  # App ID
+    v = '5.131'  # API version
+    api_session = vk.UserAPI(user_login=login, user_password=password, client_id=client_id, v=v)
+    return api_session
+
+
+def get_1000_users(api_session: vk.UserAPI, **params):
+    offset = 0
+    count = 200
+    users = []
+    while offset < 1000:
+        response = api_session.users.search(offset=offset, count=count, **params)
+        users.extend(response['items'])
+        time.sleep(0.25)
+        offset += 200
+    return users
+
+
+def save_results(users):
+    columns = set(i for d in users for i in d)
+
+    with open('out.csv', 'w', newline='') as f:
+        writer = csv.DictWriter(f, fieldnames=columns)
+        writer.writeheader()
+        for row in users:
+            writer.writerow(row)
+
+
+if __name__ == '__main__':
+    user_login = '*'
+    user_pass = '*'
+
+    api = authorize_in_app(user_login, user_pass)
+
+    fields = [
+        'city',
+        'photo_max_orig'
+    ]
+
+    results = get_1000_users(api, q='Sergey', has_photo=1, fields=fields)
+    save_results(results)
